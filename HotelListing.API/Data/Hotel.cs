0f6797254b8f47f9de94511386d25c0e9ae8b478
Wiem <chalouatiwiem01@gmail.com>
2023-08-18 14:34:05 +0100

    Add Entity Framework and create DB

---
+++ HotelListing.API/Data/Hotel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotelListing.API.Data
+{
+    public class Hotel
+    {
+
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Adress { get; set; }
+        public Double Rating { get; set; }
+
+        [ForeignKey(nameof(CountryId))]
+        public int CountryId { get; set; }
+        public Country Country { get; set; }
+    }
+}
